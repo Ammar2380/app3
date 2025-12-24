27dc1912a14c2cf8dd6e71709cfeeb4416aab83d
Ammar Jamali <ammar211132@gmail.com>
2025-12-24 19:19:28 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './', // <-- relative paths for assets
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: './', // ✅ important for relative paths in production (Vercel)
 })
